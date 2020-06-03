87e64f70e90d0c85acea3e1d178e9affc487f05b
Gustavo Araujo <gustavoaraujo600@hotmail.com>
2020-06-03 20:51:55 -0300

    backend

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const routes = express.Router();
+const pointController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+
+routes.get("/points", pointController.index);
+routes.get("/points/:id", pointController.show);
+routes.post("/points", pointController.create);
+
+export default routes;
