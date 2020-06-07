71f0fa184403a0be34b6a502aa93acb372528b24
Gustavo Araujo <gustavoaraujo600@hotmail.com>
2020-06-07 13:12:06 -0300

    Finale!.

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -9,6 +9,8 @@ import api from "../../services/api";
 import "./styles.css";
 import Logo from "../../assets/logo.svg";
 
+import Dropzone from "../../components/Dropzone";
+
 interface Item {
   id: number;
   title: string;
@@ -28,10 +30,10 @@ const CreatePoint = () => {
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
 
-  // const [initialPosition, setInitialPosition] = useState<[number, number]>([
-  //   0,
-  //   0,
-  // ]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
 
   const [formData, setFormData] = useState({
     name: "",
@@ -46,8 +48,17 @@ const CreatePoint = () => {
     0,
   ]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
 
   useEffect(() => {
     api.get("items").then((response) => {
@@ -80,14 +91,6 @@ const CreatePoint = () => {
         setCities(cityNames);
       });
   }, [selectedUf]);
-
-  // useEffect(() => {
-  //   navigator.geolocation.getCurrentPosition((position) => {
-  //     const { latitude, longitude } = position.coords;
-
-  //     setInitialPosition([latitude, longitude]);
-  //   });
-  // }, []);
 
   function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
@@ -127,16 +130,17 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+    if (selectedFile) data.append("image", selectedFile);
 
     await api.post("points", data);
 
@@ -161,6 +165,8 @@ const CreatePoint = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
